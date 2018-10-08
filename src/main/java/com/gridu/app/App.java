1c1259126f266e736d98d23fbcdaf3e90fa8182d
Egor A. Fisher <efisher@griddynamics.com>
2018-10-08 16:14:52 +0300

    go on

---
+++ src/main/java/com/gridu/app/App.java
@@ -0,0 +1,17 @@
+package com.gridu.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+
